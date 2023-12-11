284327501b71f37a99537143cdb30f08adcf5f18
Wasiu-hub <contactbow@yahoo.com>
2023-12-11 01:07:22 +0000

    running prerequisite test case

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 public class MyTest {
@@ -10,5 +11,10 @@ public class MyTest {
     @Test
     public void test2(){
         System.out.println("Test 2 is executing");
+    }
+
+    @Before
+    public void before(){
+        System.out.println("Before executing every test case");
     }
 }
