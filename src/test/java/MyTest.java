9c7ea51135390e2738dbcd976afba2e5660515a1
Wasiu-hub <contactbow@yahoo.com>
2023-12-11 00:14:23 +0000

    first test cases executed

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+public class MyTest {
+
+    @Test
+    public void test1(){
+        System.out.println("Test 1 is executing");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Test 2 is executing");
+    }
+}
